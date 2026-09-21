b445ee87f7299e40d1677185748206dc97278103
GLCN1234 <globalleadershipcn@gmail.com>
2026-09-21 19:44:36 +0000

    Remove leftover 3D test button from Settings and obsolete Beta game prototypes from Edu Gaming

--- lib/features/edu/edu_home_screen.dart
+++ lib/features/edu/edu_home_screen.dart
@@ -43,12 +43,10 @@ class _EduHomeState extends State<EduHomeScreen> {
   ];
 
   final _quickGames = [
-    {'name': 'Astra Colony (Beta)', 'icon': Icons.explore_rounded, 'route': '/arena/practice/astracolony'},
     {'name': 'Signal Run', 'icon': Icons.directions_run_rounded, 'route': '/arena/practice/endlessrunner'},
     {'name': 'Drone Breach', 'icon': Icons.gps_fixed_rounded, 'route': '/arena/practice/dronebreach'},
     {'name': 'Signal Match', 'icon': Icons.gesture_rounded, 'route': '/arena/practice/signalmatch'},
     {'name': 'Vault Break', 'icon': Icons.rotate_right_rounded, 'route': '/arena/practice/vaultbreak'},
-    {'name': 'Field Trial (Beta)', 'icon': Icons.grid_view_rounded, 'route': '/arena/practice/spatialquiz'},
     {'name': 'Colony Siege', 'icon': Icons.view_in_ar_rounded,   'route': '/arena/practice/colonybuilder'},
     {'name': 'Arena Gauntlet',    'icon': Icons.shield_rounded, 'route': '/arena/practice/weaponduel'},
     {'name': 'Speed Math',    'icon': Icons.bolt_rounded,          'route': '/arena/practice/speedmath'},
